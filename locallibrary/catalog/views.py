23b2e3b2bc34a0cbf50a45c9dd657ea9bfb456da
Cameron Sateri <satericj@dukes.jmu.edu>
2025-10-20 14:02:19 -0400

    t

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -18,11 +18,18 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # (LAB09)
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits, # (LAB09)
     }
 
     # Render the HTML template index.html with the data in the context variable
