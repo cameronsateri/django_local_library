c5883ef5b4006524b254d3ec953bcbf6cbd1a8bc
Cameron Sateri <satericj@dukes.jmu.edu>
2025-11-07 14:37:36 -0500

    lab 11

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -22,3 +22,15 @@ urlpatterns = [
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
+# (LAB11)
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+# (LAB11)
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
